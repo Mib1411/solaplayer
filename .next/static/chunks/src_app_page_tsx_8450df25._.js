cfa89159b56190f3d7398b9ffda8e913e45d6875
Michael Benze <solarism@me.com>
2025-06-12 16:15:17 +0200

    Remove .next directory from git tracking

--- .next/static/chunks/src_app_page_tsx_8450df25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_page_module_d12eb71c.css",
-  "static/chunks/node_modules_next_dist_b758c999._.js"
-],
-    source: "dynamic"
-});
